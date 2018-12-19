cb523061a4928fad3f4e0f74fae5ef4318c207a2
JeongSikKim88 <jackslash88@gmail.com8>
2018-12-19 17:59:56 +0900

    Initial Commit

---
+++ practice/practice/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  practice
+//
+//  Created by JeongSik Kim on 2018-12-06.
+//  Copyright © 2018 ___Jack___. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+
+}
